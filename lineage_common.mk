f7db59f4dc4539ef5c96caca45ef7bbfab794d8f
nhuuhuy634 <daxua1234567@gmail.com>
2023-11-12 22:54:16 +0700

    initial gs101 platform

---
+++ lineage_common.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-lineage
+
+# AiAi Config
+PRODUCT_COPY_FILES += \
+    device/google/gs101/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
+
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
+
+# EUICC
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
+# Lineage Health
+include hardware/google/pixel/lineage_health/device.mk
+
+# Parts
+PRODUCT_PACKAGES += \
+    GoogleParts
+
+# Touch
+include hardware/google/pixel/touch/device.mk
